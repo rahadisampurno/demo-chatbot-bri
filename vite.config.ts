5b57c1dfe9eb4239360072497475c8d691b45a6c
telkom-dev <rahadi.rahasia@gmail.com>
2025-07-11 22:42:12 +0700

    github page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/demo-chatbot-bri/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
